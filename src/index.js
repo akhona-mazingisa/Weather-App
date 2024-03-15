5c4e32ea8352db8941384dbab1036378156a45ab
akhona-mazingisa <akhonamazingisa04@gmail.com>
2024-03-15 10:20:47 +0200

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,30 @@ root.render(
   <StrictMode>
     <div className="App">
       <h1>Weather App</h1>
+      <div class="container">
+      <nav class="d-flex justify-content-between">
+        <a href="/" title="Homepage">
+        </a>
+        <ul>
+          <li>
+            <a href="/" title="Homepage"> Home</a>
+          </li>
+          <li class="active">
+            <a href="/about.html" title="Lisbon"> Lisbon</a>
+          </li>
+          <li>
+            <a href="/work.html" title="Paris"> Paris</a>
+          </li>
+          <li>
+            <a href="/contact.html" title="Sydney"> Sydney</a>
+          </li>
+          <li>
+            <a href="/contact.html" title="San Francisco"> San Francisco</a>
+          </li>
+        </ul>
+      </nav>
       <WeatherSearch />
+      </div>
     </div>
-  </StrictMode>
+      </StrictMode>
 );
